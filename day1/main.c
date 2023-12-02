63c52f090ed70fc33787c6ed0443a96bc9cf13f3
gcarrillo <gabriel.carrillo@usach.cl>
2023-12-02 01:43:31 -0300

    day1: fixed tabs

--- day1/main.c
+++ day1/main.c
@@ -6,30 +6,30 @@
 
 int main(int argc, char** argv){
 
-		  FILE* input = stdin;
-		  int items = 0;
-		  char buffer[MAXBUFFER] = "";
-		  int sum = 0;
-		  char ns[3] = "";
-
-		  do{
-					 items = fscanf(stdin, "%s\n", buffer);
-					 if (items>0){
-								for(int x = 0; x<strlen(buffer); x++){
-										  if(buffer[x] >= '0' && buffer[x]<='9'){
-													 ns[0] = buffer[x];
-													 break;
-										  }
-								}
-								for(int x = strlen(buffer); x>=0; x--){
-										  if(buffer[x] >= '0' && buffer[x]<='9'){
-													 ns[1] = buffer[x];
-													 break;
-										  }
-								}
-								sum += atoi(ns);
-								
-					 }
-		  }while(items>0);
-		  printf("%d\n", sum);
+	FILE* input = stdin;
+	int items = 0;
+	char buffer[MAXBUFFER] = "";
+	int sum = 0;
+	char ns[3] = "";
+	
+	do{
+		items = fscanf(stdin, "%s\n", buffer);
+		if (items>0){
+			for(int x = 0; x<strlen(buffer); x++){
+				if(buffer[x] >= '0' && buffer[x]<='9'){
+					ns[0] = buffer[x];
+					break;
+				}
+			}
+			for(int x = strlen(buffer); x>=0; x--){
+				if(buffer[x] >= '0' && buffer[x]<='9'){
+					ns[1] = buffer[x];
+					break;
+				}
+			}
+			sum += atoi(ns);
+		       		
+		}
+	}while(items>0);
+	printf("%d\n", sum);
 }
